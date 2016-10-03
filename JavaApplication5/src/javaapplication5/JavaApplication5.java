c08beeedc581af27b927a3ab63abf300fcc6be20
Ruslan <hanzhukruslan@gmail.com>
2016-10-03 12:53:42 +0300

    getStart

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,13 @@
+package javaapplication5;
+
+public class JavaApplication5 {
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 1;
+        int c = a + b;
+        
+        System.out.println(c);
+    }
+    
+}
